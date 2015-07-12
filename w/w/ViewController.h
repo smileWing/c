c259b4c700b4c32b6b9e2e54df99677f1b950d48
smileWing <zhanglei@zhangleideMacBook-Air.local>
2015-07-12 15:24:15 +0800

    xx
    xx

---
+++ w/w/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  w
+//
+//  Created by 张磊 on 15/7/12.
+//  Copyright (c) 2015年 wing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ w/w/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  w
+//
+//  Created by 张磊 on 15/7/12.
+//  Copyright (c) 2015年 wing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
